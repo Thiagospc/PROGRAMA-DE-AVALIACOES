de8c6704cb7ba5329f481cdefbcdde9c85da1dfc
Thiago Santos <64646796+Thiagospc@users.noreply.github.com>
2021-07-07 18:24:21 -0300

    Update Programa.py
    Primeira versão do programa

--- Programa.py
+++ Programa.py
@@ -1,20 +1,3 @@
-'''
-09-Aqui na UFRA existem 3 avaliaçõesregulares, O 1º NAP , o 2º NAP e a prova Substitutiva.
-Para o aluno passar direto ele precisa alcançar media maior ou igual a 6 nas duas
-maiores notas entre essas três avaliações. O aluno só́pode ficar de recuperação se ele
-conseguir media maior ou igual a 4 nas duas maiores notas dessas três avaliações. Se o
-aluno ficar de recuperação ele deve pegar a media das avaliações regulares (calculada
-entre as 2 maiores notas) e somar com o NAF ( nota de avaliação final ) e calcular a
-media entre essas duas notas , se a nota obtida for maior ou igual a 6 o aluno está
-aprovado. Com base no sistema de notas da UFRA, faça um algoritmos que o usuário
-digite as 3 notas regulares do aluno, a seguir o algoritmo deve informar se :
-• O aluno foi aprovado,
-• Se o aluno já está reprovado
-• Ou se o aluno ficou de recuperação. Se o aluno ficou de recuperação informe
-quanto ele precisa tirar no NAF para passar.
-Dica: pesquise pela função min ( mínimo ) que pega o menor valor de uma lista de
-valores. Use o termo python min no seu buscador da web.
-'''
 from time import sleep
 print('<__________Programa de avaliações UFRA__________>')
 print()
